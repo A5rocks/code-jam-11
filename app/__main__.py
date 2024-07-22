9fe11ec2818795fc5968168105e676369f950aed
Darkshadogt <functionaljasonchen@gmail.com>
2024-07-22 16:21:00 -0400

    Polished commands

--- app/__main__.py
+++ app/__main__.py
@@ -24,6 +24,34 @@ class DiscordClient(discord.Client):
         await self.tree.sync()
 
 
+class Config(app_commands.Group):
+    """Custom subclass of AppCommandGroup for config commands."""
+
+    @app_commands.command()
+    async def enable(self, interaction: discord.Interaction) -> None:
+        """Enable the game on the current channel."""
+        if interaction.user.guild_permissions.manage_guild:
+            await interaction.response.send_message("Enabling the game on this channel")
+        else:
+            await interaction.response.send_message("You don't have permission to do that")
+
+    @app_commands.command()
+    async def disable(self, interaction: discord.Interaction) -> None:
+        """Disable the game on the current channel."""
+        if interaction.user.guild_permissions.manage_guild:
+            await interaction.response.send_message("Disabling the game on this channel")
+        else:
+            await interaction.response.send_message("You don't have permission to do that")
+
+    @app_commands.command()
+    async def reset(self, interaction: discord.Interaction) -> None:
+        """Reset access to the game for all channels."""
+        if interaction.user.guild_permissions.manage_guild:
+            await interaction.response.send_message("Resetting all channels access")
+        else:
+            await interaction.response.send_message("You don't have permission to do that")
+
+
 client = DiscordClient(intents=discord.Intents.default())
 
 
@@ -38,14 +66,8 @@ async def repeat(interaction: discord.Interaction, what: str) -> None:
 
 
 @client.tree.command()
-async def enable(interaction: discord.Interaction) -> None:
-    """Enable the game on the current channel."""
-    await interaction.response.send_message("Enabling the game on this channel")
-
-
-@client.tree.command()
 @app_commands.describe(message="The message to send")
-async def message(interaction: discord.Interaction, message: str) -> None:
+async def send(interaction: discord.Interaction, message: str) -> None:
     """Send a message to the current channel."""
     await interaction.response.send_message(message)
 
@@ -55,6 +77,9 @@ async def upgrade(interaction: discord.Interaction) -> None:
     """Upgrade."""
     await interaction.response.send_message("Upgraded")
 
+
+config = Config(name="config", description="Configures the game")
+client.tree.add_command(config)
 
 if __name__ == "__main__":
     client.run(TOKEN)
