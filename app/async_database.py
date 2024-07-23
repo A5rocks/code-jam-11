67ad2d08d8db2b3a295b67de37fe0e105c5c27d9
aceeedev <cyberandrew03@gmail.com>
2024-07-22 17:28:56 -0700

    Cleaned up code and made shared db connection

--- app/async_database.py
+++ app/async_database.py
@@ -1,43 +1,18 @@
+import contextlib
+import typing
+
 import aiosqlite
 import discord
 from database import UserProfile
+
+db_path = "demo.db"
 
 
 class AsyncDatabase:
     """Class to store user profile and channel data asynchronously."""
 
-    db_path = "demo.db"
-
-    async def setup(self) -> None:
-        """Init for all asynchronous work for database."""
-        async with aiosqlite.connect(self.db_path) as db:
-            # initialize tables
-            await db.execute("""CREATE TABLE IF NOT EXISTS Guilds (
-                                id int,
-                                channel int,
-                                enabled bool)""")
-            await db.commit()
-            await db.execute("""CREATE TABLE IF NOT EXISTS Users (
-                                user_id int,
-                                guild_id int,
-                                cps float,
-                                coins int)""")
-            await db.commit()
-
-    async def add_guild(self, guild: discord.Guild) -> None:
-        """Add a guild to the database.
-
-        Arguments:
-        ---------
-        guild (discord.Guild): The guild that the user is in
-
-        """
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute(
-                "INSERT INTO Guilds(id, channel, enabled) VALUES (?,?,?)",
-                (guild.id, 0, False),
-            )
-            await db.commit()
+    def __init__(self, connection: aiosqlite.Connection) -> None:
+        self.connection = connection
 
     async def enable_channel(self, channel: discord.TextChannel) -> None:
         """Enable the game in a channel.
@@ -47,14 +22,11 @@ class AsyncDatabase:
         channel (discord.TextChannel): The channel that the game is to be enabled in
 
         """
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute(
-                """UPDATE Guilds
-                                SET channel = ?, enabled= ?
-                                WHERE id = ?""",
-                (channel.id, True, channel.guild.id),
-            )
-        await db.commit()
+        await self.connection.execute(
+            "INSERT INTO Guilds(id, channel) VALUES (?,?)",
+            (channel.guild.id, channel.id),
+        )
+        await self.connection.commit()
 
     async def disable_channel(self, channel: discord.TextChannel) -> None:
         """Disable the game in a channel.
@@ -64,14 +36,8 @@ class AsyncDatabase:
         channel (discord.TextChannel): The channel that the game is to be disabled in
 
         """
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute(
-                """UPDATE Guilds
-                                SET channel = ?, enabled= ?
-                                WHERE id = ?""",
-                (channel.id, False, channel.guild.id),
-            )
-        await db.commit()
+        await self.connection.execute("DELETE FROM Guilds WHERE id=? AND channel=?", (channel.guild.id, channel.id))
+        await self.connection.commit()
 
     async def get_active_profiles(self, guild: discord.Guild) -> list[UserProfile]:
         """Get all the profiles that the game is in enabled in.
@@ -81,15 +47,11 @@ class AsyncDatabase:
         guild (discord.Guild): The guild to check all enabled profiles in
 
         """
-        active_profiles: list[UserProfile] = []
-
         async with (
-            aiosqlite.connect(self.db_path) as db,
+            aiosqlite.connect(db_path) as db,
             db.execute("SELECT * FROM Users WHERE guild_id = ?", (guild.id)) as cursor,
         ):
-            async for row in cursor:
-                active_profiles = [UserProfile(coins=row[3], cps=row[2]) for row in cursor]
-        return active_profiles
+            return [UserProfile(coins=row[3], cps=row[2]) for row in cursor]
 
     async def add_profile(self, guild: discord.Guild, user: discord.Member) -> None:
         """Add a profile to a specific guild.
@@ -100,12 +62,11 @@ class AsyncDatabase:
         user (discord.Member): This user whose profile is to be added
 
         """
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute(
-                "INSERT INTO Users(user_id, guild_id, cps, coins) VALUES (?,?,?,?)",
-                (user.id, guild.id, UserProfile().cps, UserProfile().coins),
-            )
-            await db.commit()
+        await self.connection.execute(
+            "INSERT INTO Users(user_id, guild_id, cps, coins) VALUES (?,?,?,?)",
+            (user.id, guild.id, UserProfile().cps, UserProfile().coins),
+        )
+        await self.connection.commit()
 
     async def remove_profile(self, guild: discord.Guild, user: discord.Member) -> None:
         """Remove a profile from a specific guild.
@@ -116,9 +77,8 @@ class AsyncDatabase:
         user (discord.Member): This user whose profile is to be removed
 
         """
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute("DELETE FROM Users WHERE user_id=? AND guild_id=?", (user.id, guild.id))
-            await db.commit()
+        await self.connection.execute("DELETE FROM Users WHERE user_id=? AND guild_id=?", (user.id, guild.id))
+        await self.connection.commit()
 
     async def get_profile(self, guild: discord.Guild, user: discord.Member) -> UserProfile | None:
         """Get a profile from a specific guild, if the user object does not have the guild already attached to it.
@@ -130,7 +90,7 @@ class AsyncDatabase:
 
         """
         async with (
-            aiosqlite.connect(self.db_path) as db,
+            aiosqlite.connect(db_path) as db,
             db.execute("SELECT * FROM Users WHERE guild_id = ? AND user_id = ?", (guild.id, user.id)) as cursor,
         ):
             async for row in cursor:
@@ -155,11 +115,35 @@ class AsyncDatabase:
         If this is not passed in a value of None is used and the profile is not changed
 
         """
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute(
-                """UPDATE Users
-                                SET cps = ?, coins = ?
-                                WHERE user_id = ? AND guild_id= ?""",
-                (new_profile.cps, new_profile.coins, user.id, guild.id),
-            )
+        await self.connection.execute(
+            """UPDATE Users
+                            SET cps = ?, coins = ?
+                            WHERE user_id = ? AND guild_id= ?""",
+            (new_profile.cps, new_profile.coins, user.id, guild.id),
+        )
+        await self.connection.commit()
+
+
+@contextlib.asynccontextmanager
+async def open_database(path: str) -> typing.Generator[AsyncDatabase]:
+    """Open a database through a shared connection.
+
+    Arguments:
+    ---------
+    path (str): The path of the database to open
+
+    """
+    async with aiosqlite.connect(path) as db:
+        # initialize tables if needed
+        await db.execute("""CREATE TABLE IF NOT EXISTS Guilds (
+                            id int,
+                            channel int) STRICT""")
         await db.commit()
+        await db.execute("""CREATE TABLE IF NOT EXISTS Users (
+                            user_id int,
+                            guild_id int,
+                            cps float,
+                            coins int) STRICT""")
+        await db.commit()
+
+        yield AsyncDatabase(db)
