e4795c2a37b3c6a2185e7c2997a10375d2518271
LuckElixir <sriniranjan.gs@gmail.com>
2024-07-24 12:17:15 -0400

    Added a persistent UpgradeView and embed with the upgrade command for user interaction, allowing for priority and cps upgrade

--- app/__main__.py
+++ app/__main__.py
@@ -3,9 +3,55 @@ import os
 import discord
 import dotenv
 from discord import app_commands
+from discord.ui import Button, View
 
 dotenv.load_dotenv()
 TOKEN = os.environ["TOKEN"]
+
+
+class UpgradeView(View):
+    """A discord.View subclass to handle user interactions with the update screen."""
+
+    def __init__(self) -> None:
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="Upgrade CPS", style=discord.ButtonStyle.blurple, custom_id="upgradepersistent:cps")
+    async def cps_upgrade(self, interaction: discord.Interaction, button: Button) -> None:
+        """Upgrade a user's CPS."""
+        button.label = "CPS Selected"
+        if interaction.message is not None:
+            await interaction.message.edit(view=self)
+        await interaction.response.send_message(
+            "Dummy message confirming CPS method works",
+            ephemeral=True,
+        )
+
+    @discord.ui.button(
+        label="Upgrade Priority",
+        style=discord.ButtonStyle.blurple,
+        custom_id="upgradepersistent:priority",
+    )
+    async def priority_upgrade(self, interaction: discord.Interaction, button: Button) -> None:
+        """Upgrade a user's priority."""
+        button.label = "Priority Selected"
+        if interaction.message is not None:
+            await interaction.message.edit(view=self)
+        await interaction.response.send_message(
+            "Dummy message confirming priority upgrade method works",
+            ephemeral=True,
+        )
+
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red, custom_id="upgradepersistent:cancel")
+    async def cancel(self, interaction: discord.Interaction, button: Button) -> None:
+        """Cancel the upgrade process."""
+        button.label = "Cancelled"
+        self.stop()
+        if interaction.message is not None:
+            await interaction.message.edit(content="Cancelled.", view=None)
+        await interaction.response.send_message(
+            "Stopped the view to signify cancel method",
+            ephemeral=True,
+        )
 
 
 class DiscordClient(discord.Client):
@@ -21,6 +67,7 @@ class DiscordClient(discord.Client):
 
         Right now this just synchronizes our application commands with Discord.
         """
+        self.add_view(UpgradeView())
         await self.tree.sync()
 
 
@@ -53,7 +100,11 @@ async def message(interaction: discord.Interaction, message: str) -> None:
 @client.tree.command()
 async def upgrade(interaction: discord.Interaction) -> None:
     """Upgrade."""
-    await interaction.response.send_message("Upgraded")
+    embed = discord.Embed(title="Upgrade menu", description="Select an upgrade to obtain")
+    embed.add_field(name="Better CPS", value="Increase the amount of characters you can send per second")
+    embed.add_field(name="Higher Priority", value="Increase the priority of your messages")
+    view = UpgradeView()
+    await interaction.response.send_message(embed=embed, view=view)
 
 
 if __name__ == "__main__":
